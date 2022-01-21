d399bc24338d232fa59e0e51e0fe604bdb77e7d0
Markus Staab <m.staab@complex-it.de>
2022-01-21 13:28:51 +0100

    schema

--- bootstrap.php
+++ bootstrap.php
@@ -2,3 +2,19 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
+if (false !== getenv('GITHUB_ACTION')) {
+    $dbAdapter = Zend_Db::factory('Mysqli', array(
+        'host'     => '127.0.0.1',
+        'username' => 'root',
+        'password' => 'root',
+        'dbname'   => 'zf'
+    ));
+} else {
+    $dbAdapter = Zend_Db::factory('Mysqli', array(
+        'host'     => 'mysql57.ab',
+        'username' => 'testuser',
+        'password' => 'test',
+        'dbname'   => 'mbw'
+    ));
+}
+Zend_Db_Table::setDefaultAdapter($dbAdapter);
